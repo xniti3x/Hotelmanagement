371dcd149342a7d3f6d9a430fc2329e3b8ea1a95
xniti3x <niti_3@hotmail.de>
2021-12-13 14:46:59 +0100

    remove btn calc and make item copy add item class

--- application/modules/invoices/views/view.php
+++ application/modules/invoices/views/view.php
@@ -26,16 +26,13 @@ $cv = $this->controller->view_data["custom_values"];
             );
         });
 
-        $('.btn_calc').click(function () {
-            console.log("asd");
-        });
 
         $('.btn_add_row').click(function () {
             $('#new_row').clone().appendTo('#item_table').removeAttr('id').addClass('item').show();
         });
 
         $('.btn_copy_row').click(function () {
-            $('#item_table tbody:last').clone().appendTo('#item_table');
+            $('#item_table tbody:last').clone().appendTo('#item_table').removeAttr('id').addClass('item').show();
         });
 
 
