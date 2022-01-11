5e854bdd30c21df9b4cc9881cb75c1110994a89f
xniti3x <niti_3@hotmail.de>
2022-01-11 17:50:48 +0100

    add timesheet with fullcalendar js

---
+++ application/modules/employee/views/report_modal.php
@@ -0,0 +1,8 @@
+<div class="form-group">
+    <label for="start">Start</label>
+    <input type="date" class="form-control" id="start"  placeholder="start">
+</div>
+<div class="form-group">
+    <label for="end">End</label>
+    <input type="date" class="form-control" id="end"  placeholder="end">
+</div>
--- application/modules/employee/views/timesheet_template.php
+++ application/modules/employee/views/timesheet_template.php
@@ -1,5 +1,72 @@
-<table width="100%">
-    <tr><th>Start</th><th>End</th><th>Duration</th><th>Notes</th></tr>
+<style>
+table.minimalistBlack {
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+}
+table.minimalistBlack td, table.minimalistBlack th {
+  border: 1px solid #000000;
+  padding: 5px 4px;
+}
+table.minimalistBlack tbody td {
+    font-size: 13px;
+}
+table.minimalistBlack thead {
+  background: #CFCFCF;
+  background: -moz-linear-gradient(top, #dbdbdb 0%, #d3d3d3 66%, #CFCFCF 100%);
+  background: -webkit-linear-gradient(top, #dbdbdb 0%, #d3d3d3 66%, #CFCFCF 100%);
+  background: linear-gradient(to bottom, #dbdbdb 0%, #d3d3d3 66%, #CFCFCF 100%);
+  border-bottom: 3px solid #000000;
+}
+table.minimalistBlack thead th {
+  font-size: 15px;
+  font-weight: bold;
+  color: #000000;
+  text-align: left;
+}
+table.minimalistBlack tfoot {
+  font-size: 14px;
+  font-weight: bold;
+  color: #000000;
+  border-top: 3px solid #000000;
+}
+table.minimalistBlack tfoot td {
+  font-size: 14px;
+  background:#eee;
+}
+</style>
+<table class="minimalistBlack">
+<thead>
+<tr>
+<th>Nr</th>
+<th>Day</th>
+<th>Start</th>
+<th>End</th>
+<th>Duration</th>
+<th>Notes</th>
+</tr>
+</thead>
+<tfoot>
+<tr>
+<td></td>
+<td></td>
+<td></td>
+<td>Summe:</td>
+<td><?php print_r($total);?></td>
+<td></td>
+</tr>
+</tfoot>
+<tbody>
+<?php $i=1; foreach($employee as $emp){
+            echo ("<tr>
+            <td style='width:20px;'>".$i++."</td>
+            <td style='width:120px;'>".$tage[date('w',strtotime($emp->day))]." - ".date('d-m-Y', strtotime($emp->day))."</td>
+            <td style='width:80px;'>".$emp->start."</td>
+            <td style='width:80px;'>".$emp->end."</td>
+            <td style='width:80px;'>".$emp->duration."</td>
+            <td>".$emp->notes."</td>
+            </tr>");
+        } 
+    ?>    
+</tbody>
 </table>
-
-<?php echo $start; ?>
