fb9875354620c50ac9b0ee7f2397215f2e64aa4e
xniti3x <niti_3@hotmail.de>
2022-01-16 21:44:22 +0100

    add active flag to room

--- application/modules/setup/sql/034_1.5.11.sql
+++ application/modules/setup/sql/034_1.5.11.sql
@@ -2,6 +2,7 @@
 CREATE TABLE IF NOT EXISTS `ip_rooms` (
   `id`        INT(11) NOT NULL AUTO_INCREMENT,
   `name`      VARCHAR(50)      DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1
   PRIMARY KEY (`id`)
 )
   ENGINE = MyISAM
