9f9c66dbafa76e17715048dab6f16ade9ce0f08c
xniti3x <56844608+xniti3x@users.noreply.github.com>
2022-04-12 09:27:01 +0200

    Update Booking.php

--- application/modules/bookings/models/Booking.php
+++ application/modules/bookings/models/Booking.php
@@ -14,7 +14,7 @@ class Booking extends CI_Model
     }
 
     public function select_free_room($start,$end){
-        return $this->db->query("Select * From ip_rooms where ip_rooms.id not in (SELECT item_room FROM ip_invoice_items WHERE item_date_start < '".$end."' AND item_date_end > '".$start."')")->result(); 
+        return $this->db->query("Select * From ip_rooms where show_on_system=1 AND ip_rooms.id not in (SELECT item_room FROM ip_invoice_items WHERE item_date_start < '".$end."' AND item_date_end > '".$start."')")->result(); 
     }
     
 
