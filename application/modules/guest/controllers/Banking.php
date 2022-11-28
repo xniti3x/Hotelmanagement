64bf6e096ff129a97da9fb87505c082699fd3ea8
xniti3x <niti_3@hotmail.de>
2022-11-28 15:40:29 +0100

    set android api

--- application/modules/guest/controllers/Banking.php
+++ application/modules/guest/controllers/Banking.php
@@ -39,7 +39,25 @@ class Banking extends Base_Controller
         $this->layout->buffer('content', 'guest/transaction_view');
         $this->layout->render('layout_no_navbar');
     }
-
+    public function do_api_upload($ckey,$id){if(strcmp($ckey,$this->mdl_bank_api->getValue('ckey'))!=0) exit('No access allowed');
+        $config['upload_path']          = './uploads/';
+        $config['allowed_types']        = '*';
+        $config['max_size']             = 10024;
+        $config['max_width']            = 0;
+        $config['max_height']           = 0;
+        $this->load->library('upload', $config);
+       if ( ! $this->upload->do_upload('userfile'))
+        {
+            echo "upload failed";
+        }
+        else
+        {    
+            $data = $this->upload->data();
+            $data['transactionId']=$id;
+            $this->mdl_bank_api->saveTransactionFile($data);
+            echo "upload successfull.";
+        }       
+    }
     public function do_upload($ckey,$id){if(strcmp($ckey,$this->mdl_bank_api->getValue('ckey'))!=0) exit('No access allowed');
         $config['upload_path']          = './uploads/';
         $config['allowed_types']        = 'gif|jpg|png|pdf';
@@ -54,14 +72,20 @@ class Banking extends Base_Controller
             header("Location:".site_url("guest/banking/view/".$this->mdl_bank_api->getValue('ckey')."/".$id));
         }
         else
-        {// Array ( [upload_data] => Array ( [file_name] => 2020-mu2ff7in12.pdf [file_type] => application/pdf [file_path] => /root/workspace/Hotelmanagement/uploads/ [full_path] => /root/workspace/Hotelmanagement/uploads/2020-mu2ff7in12.pdf [raw_name] => 2020-mu2ff7in12 [orig_name] => 2020-mu2ff7in.pdf [client_name] => 2020-mu2ff7in.pdf [file_ext] => .pdf [file_size] => 53.1 [is_image] => [image_width] => [image_height] => [image_type] => [image_size_str] => ) )
-             
+        {    
             $data = $this->upload->data();
             $data['transactionId']=$id;
             $this->mdl_bank_api->saveTransactionFile($data);
             $this->session->set_flashdata('alert_success', trans('record_successfully_updated'));
             header("Location:".site_url("guest/banking/index/".$this->mdl_bank_api->getValue('ckey')));
         }       
+    }
+
+    public function getAllMinusTransactions($ckey){
+        if(strcmp($ckey,$this->mdl_bank_api->getValue('ckey'))!=0) exit('No direct script access allowed');
+        header('Content-Type: application/json');        
+        $data = $this->mdl_bank_api->getAllMinusTransactions();
+        echo json_encode($data);
     }
 
     public function do_weekly($ckey){
