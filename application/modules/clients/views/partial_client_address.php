ae0a3e803c63ff2baa5bfcdf6a398f249b3b72f5
xniti3x <niti_3@hotmail.de>
2023-06-20 09:04:24 +0200

    convert to adminlte

--- application/modules/clients/views/partial_client_address.php
+++ application/modules/clients/views/partial_client_address.php
@@ -1,28 +1,25 @@
 <?php $this->load->helper('country'); ?>
-<div class="card">
-    <div class="card-header">
-    <h3 class="card-title">
-        <a href="<?php echo site_url('clients/view/' . $client->client_id); ?>">
-            <?php _htmlsc(format_client($client)) ?>
-        </a>
-    </h3>
-    </div>
-<table class="table table-borderless table-responsive">
-    <tr>
-        <?php echo ($client->client_address_1 ? "<td>" . htmlsc($client->client_address_1) . '</td>' : ''); ?>
-        <?php echo ($client->client_address_2 ? "<td>" . htmlsc($client->client_address_2) . '</td>' : ''); ?>
-        <?php echo ($client->client_zip ? "<td>" . htmlsc($client->client_zip) . '</td>' : ''); ?>
-        <?php echo ($client->client_city ? "<td>" . htmlsc($client->client_city) . "</td>" : ''); ?>
-        <?php echo ($client->client_country ? "<td>" . htmlsc($client->client_country) . "</td>" : ''); ?>
-        <?php echo ($client->client_phone ? "<td>" . htmlsc($client->client_phone) . '</td>' : ''); ?>
-        <?php echo ($client->client_email ? "<td>" . htmlsc($client->client_email) . "</td>" : ''); ?>
-        <td style="min-width: 100px;">
-            
-        <button id="invoice_change_client" class="btn btn-default btn-flat btn-sm" data-toggle="tooltip" data-placement="bottom" title="<?php _trans('change_client'); ?>"><i class="fa fa-edit"></i></button>
-        <button id="invoice_add_client" class="btn btn-default btn-flat btn-sm" data-toggle="tooltip" title="<?php _trans('add_client'); ?>"><i class="fa fa-plus"></i></button>
-    
-        </td>
-    </tr>
+        <div class="card">
+            <table class="table table-borderless table-responsive">
+                <tr>
+                    <td><a href="<?php echo site_url('clients/view/' . $client->client_id); ?>">
+                            <?php _htmlsc(format_client($client)) ?>
+                        </a></td>
+                </tr>
+                <tr>    <?php echo ($client->client_address_1 ? "<td>" . htmlsc($client->client_address_1) . '</td>' : ''); ?>
+                    <?php echo ($client->client_address_2 ? "<td>" . htmlsc($client->client_address_2) . '</td>' : ''); ?>
+                    <?php echo ($client->client_zip ? "<td>" . htmlsc($client->client_zip) . '</td>' : ''); ?>
+                    <?php echo ($client->client_city ? "<td>" . htmlsc($client->client_city) . "</td>" : ''); ?>
+                    <?php echo ($client->client_country ? "<td>" . htmlsc($client->client_country) . "</td>" : ''); ?>
+                    <?php echo ($client->client_phone ? "<td>" . htmlsc($client->client_phone) . '</td>' : ''); ?>
+                    <?php echo ($client->client_email ? "<td>" . htmlsc($client->client_email) . "</td>" : ''); ?>
+                    <td style="min-width: 100px;">
 
-</table>
-</div>
+                        <button id="invoice_change_client" class="btn btn-default btn-flat btn-sm" data-toggle="tooltip" data-placement="bottom" title="<?php _trans('change_client'); ?>"><i class="fa fa-edit"></i></button>
+                        <button id="invoice_add_client" class="btn btn-default btn-flat btn-sm" data-toggle="tooltip" title="<?php _trans('add_client'); ?>"><i class="fa fa-plus"></i></button>
+
+                    </td>
+                
+
+            </table>
+        </div>
--- application/modules/layout/views/admin_lte/sidebar.php
+++ application/modules/layout/views/admin_lte/sidebar.php
@@ -2,7 +2,7 @@
 
     <a href="index3.html" class="brand-link">
         <img src="<?php echo base_url(); ?>assets/AdminLTE/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light"><?php echo get_setting('custom_title', null, true) ?: 'Hotelmanagement';?></span>
     </a>
 
     <div class="sidebar">
@@ -13,17 +13,6 @@
             </div>
             <div class="info">
                 <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div>
-
-        <div class="form-inline">
-            <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-sidebar">
-                        <i class="fas fa-search fa-fw"></i>
-                    </button>
-                </div>
             </div>
         </div>
 
